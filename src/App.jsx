482f82e101b513efd27e3a9f8158a98721fc9b23
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-04-02 22:06:17 +0800

    update some things

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,8 @@ import MyTodo from "./components/TodoList";
 function App() {
   return (
     <div className="App">
-      <nav className="flex justify-between p-5 font-mono border border-gray-400">
-        <h1 className="border border-gray-400">todo list ni jarwin</h1>
-
-        <ul className="flex gap-3 border border-gray-400 items-center">
-          <a href="">Github</a>
-          <a href="">Facebook</a>
-          <a href="">Instagram</a>
-        </ul>
+      <nav className="p-5 flex items-center bg-red-500 text-white">
+        <h1>Todo List ni jerwin</h1>
       </nav>
       <MyTodo/>
     </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,8 +5,9 @@ export default function MyTodo () {
 
     // state handler
     const[Input, setInput] = useState("")
-    const[Todos, setTodos] = useState(["Make a Todo List", "Read a Book", "Learn some react states"])
+    const[Todos, setTodos] = useState([])
     const[Done, dontLIst] = useState([])
+    const[date, SetDate] = useState([])
 
 
 
@@ -15,6 +16,7 @@ export default function MyTodo () {
         if(Input.trim()) {
             setTodos((t) => [...t, Input])
             setInput("")
+            SetDate((prevTime) => [...prevTime, new Date().toLocaleTimeString()])
         }
     }
 
@@ -26,34 +28,41 @@ export default function MyTodo () {
     }
 
     // handle done task
-    const doneTodo = (item) => {
-        if(Done.includes(item)) {
-            dontLIst((prev) =>  prev.filter((t) => t !== item))
+    const doneTodo = (index) => {
+        if (Done.includes(index)) {
+            dontLIst((prev) => prev.filter((i) => i !== index));
         } else {
-            dontLIst((prev) => [...prev, item])
+            dontLIst((prev) => [...prev, index]);
         }
-        
-    }
+    };
 
     return(
         <div className="p-5">
-            <div className="flex items-center mb-2 justify-between">
+            <div className="flex items-center mb-2 justify-between border border-gray-400">
                 <input type="text" 
                 value={Input}
                 onChange={(e) => setInput(e.target.value)}
-                className="p-4 border border-gray-400 flex-1"/>  
-                <button className="p-5 bg-green-500 text-white text-xs font-semibold border-none" onClick={() => getTodo()}>ADD</button>
+                className="p-4 border flex-1"/>  
+                <button className="p-5 bg-green-500 text-white text-lg font-semibold border-none" onClick={() => getTodo()}>ADD ➕</button>
             </div>
 
             {Todos && (
                 <ul>
                     {Todos.map((item, index) => (
-                        <li key={index} className={Done.includes(item) ? 'p-3 flex justify-between items-center mb-1 bg-green-500 text-white' : 'p-3 flex justify-between border border-gray-400 items-center mb-1'}>
-                            {item} 
-                            <span className="flex gap-2">
-                                <button className={Done.includes(item) ? 'bg-blue-500 text-white p-2' : 'bg-green-500 text-white p-2'} onClick={() => doneTodo(item)}>Done</button>
-                                <button className="bg-red-500 text-white p-2" onClick={() => deleteTodo(index)}>Delete</button>
+                        <li key={index} className={Done.includes(index) ? 'p-4 flex justify-between mb-1 bg-green-500 text-white' : 'p-3 flex justify-between border border-gray-400 mb-1'}>
+
+                            <span>
+                                <p>{item} {Done.includes(index) ? '(Task Done ✅)' : ''}</p>
+                                <p>{date[index]}</p>
                             </span>
+                             
+                            <span className="flex gap-2">
+                                <button className={Done.includes(index) ? 'bg-blue-500 text-white p-2' : 'bg-green-500 text-white p-2'} onClick={() => doneTodo(index)}>
+                                    {Done.includes(index) ? 'Undo ❌' : 'Done ✅'}
+                                </button>
+                                <button className="bg-red-500 text-white p-2" onClick={() => deleteTodo(index)}>Delete 🗑️</button>
+                            </span>
+                            
                         </li>
                     ))}
                 </ul>
